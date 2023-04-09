dcff6adb0123c57d47d6686f367db64d0d7cc14e
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2023-04-09 23:33:21 +0300

    Initial commit

---
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -0,0 +1,70 @@
+package com.bookit.step_definitions;
+
+import com.bookit.utilities.BookItApiUtil;
+import com.bookit.utilities.ConfigurationReader;
+import com.bookit.utilities.DBUtils;
+import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
+public class ApiStepDefs {
+//0.40
+ String token;
+ Response response;
+ String emailGlobal;
+    @Given("I logged Bookit api using {string} and {string}")
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+         token = BookItApiUtil.generateToken(email, password);
+         emailGlobal=email;
+    }
+
+    @When("I get the current user information from api")
+    public void i_get_the_current_user_information_from_api() {
+        System.out.println("token = " + token);
+        //send a GET request "/api/users/me" endpoint to get current user info
+    response=    given().contentType(ContentType.JSON)
+                .and().header("Authorzation",token)
+                .get(ConfigurationReader.get("base_url")+"/api/users/me");
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(int statusCode) {
+        Assert.assertEquals(statusCode,response.statusCode());
+    }
+
+    @Then("the information about current user from api and database should match")
+    public void theInformationAboutCurrentUserFromApiAndDatabaseShouldMatch() {
+        System.out.println("we will compare database and api in this step");
+
+        //get information from database
+        //connection is from hooks and it will be ready
+        String query = "select firstname,lastname,role from users\n" +
+                "where email = '"+emailGlobal+"'";
+
+        Map<String,Object> dbMap = DBUtils.getRowMap(query);
+        System.out.println("dbMap = " + dbMap);
+        //save db info into variables
+        String expectedFirstName = (String) dbMap.get("firstname");
+        String expectedLastName = (String) dbMap.get("lastname");
+        String expectedRole = (String) dbMap.get("role");
+
+        //get information from api
+        JsonPath jsonPath = response.jsonPath();
+        //save api info into variables
+        String actualFirstName = jsonPath.getString("firstName");
+        String actualLastName = jsonPath.getString("lastName");
+        String actualRole = jsonPath.getString("role");
+
+        //compare database vs api
+        Assert.assertEquals(expectedFirstName,actualFirstName);
+        Assert.assertEquals(expectedLastName,actualLastName);
+        Assert.assertEquals(expectedRole,actualRole);
+    }
+}
